274a690857d43b95dbf4d3c59f3aec7ed4004200
murinag0ston23-byte <muirnag0ston23@gmail.com>
2026-07-06 14:28:35 +0200

    Első feltöltés

---
+++ main.js
@@ -0,0 +1,14 @@
+let age=prompt('Hány éves vagy?',18);
+  if (age < 18) {
+    alert('fiatal')
+  }else{
+    alert('felnőtt')
+  }
+
+let cimsor= document.getElementById("nevem");
+cimsor.innerHTML="Murin Ágoston László";
+cimsor.style.backgroundColor= "red";
+cimsor.style.color= "white";
+cimsor.style.display= "inline-block";
+
+console.log('szia')
